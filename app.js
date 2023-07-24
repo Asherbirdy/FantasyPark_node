6d727a82054bdcd90e122895fe87ee9e7abe1a04
Asher Lien <birdym.tw@gmail.com>
2023-07-24 14:59:54 +0800

    建立 server 設定

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+// Express
+const express = require('express');
+const app = express();
+
+// Rest of the package
+const morgan = require('morgan');
+
+// Database
+const connectDB = require('./db/connect');
+
+// Middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerrMiddleware = require('./middleware/error-handler');
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Fantasy API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerrMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
