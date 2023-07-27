3aa663300645162014896c688d052b59602fa316
Asherbirdy <birdym.tw@gmail.com>
2023-07-27 08:19:24 +0800

    update password controller

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const app = express();
 // Rest of the package
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+// const cors = require('cors');
 // Database
 const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // Middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -19,7 +21,7 @@ const errorHandlerrMiddleware = require('./middleware/error-handler');
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
-
+// app.use(cors);
 app.get('/', (req, res) => {
   res.send('Fantasy API');
 });
@@ -30,6 +32,7 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerrMiddleware);
---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const getAllUsers = async (req, res) => {
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSingleUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+
+  if (!user) {
+    throw new CustomError.NotFoundError(
+      `No user found with id: ${req.params.id}`
+    );
+  }
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const showCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+
+const updateUser = async (req, res) => {
+  res.send(req.body);
+};
+
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError('Please provide both values');
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPassword = await user.comparePassword(oldPassword);
+  if (!isPassword) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  user.password = newPassword;
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: 'Sucess! Password Updated' });
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+};
