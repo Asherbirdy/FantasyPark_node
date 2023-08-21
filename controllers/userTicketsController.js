089c7cbd91513e819676afc6f72e7d570f9cc2b6
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-08-21 19:45:02 +0800

    修改getuserTicket新增時間參數 (#10)
    * 完成
    
    * 時間:
    
    進度備份

--- controllers/userTicketsController.js
+++ controllers/userTicketsController.js
@@ -93,13 +93,17 @@ const getUnuseUseTickets = async (req, res) => {
   const ticketDateAsString = findTodayUnuseTicket[0]?.ticketDate.toISOString();
 
   if (findTodayUnuseTicket.length > 0 && ticketDateAsString === todayDate) {
-    res
-      .status(StatusCodes.OK)
-      .json({ findTodayUnuseTicket, count: findTodayUnuseTicket.length });
+    res.status(StatusCodes.OK).json({
+      findTodayUnuseTicket,
+      count: findTodayUnuseTicket.length,
+      ticketDate: findTodayUnuseTicket[0]?.ticketDate,
+    });
   } else {
-    res
-      .status(StatusCodes.OK)
-      .json({ findUnuseTicket, count: findUnuseTicket.length });
+    res.status(StatusCodes.OK).json({
+      findUnuseTicket,
+      count: findUnuseTicket.length,
+      ticketDate: findUnuseTicket[0]?.ticketDate,
+    });
   }
 };
 
@@ -109,7 +113,7 @@ const getFilteredTickets = async (filter) => {
       path: 'ticketCategoryId',
       select: '_id ticketType fastTrack price',
     })
-    .select('-createdAt -updatedAt -__v -userId -statusDate -purchaseDate')
+    .select('-createdAt -updatedAt -__v -userId ')
     .lean();
 };
 
