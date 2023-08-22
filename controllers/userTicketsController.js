3da7a4cc9183d1e18083107a9dedd2f9ec993cda
Asher Lien <birdym.tw@gmail.com>
2023-08-22 16:12:45 +0800

    完成api

--- controllers/userTicketsController.js
+++ controllers/userTicketsController.js
@@ -110,7 +110,16 @@ const getFilteredTickets = async (filter) => {
 };
 
 const getUserUsedRefundExpiredTicketHistory = async (req, res) => {
-  res.send('getUserUsedRefundExpiredTicketHistory');
+  const ticketHistroy = await UsersTickets.find({
+    userId: req.user.userId,
+    status: { $in: ['expired', 'refund', 'used'] },
+  })
+    .select(
+      'ticketDate status ticketCategoryId currentPurchasePrice statusDate'
+    )
+    .sort({ statusDate: -1 });
+  console.log(ticketHistroy.length);
+  res.status(StatusCodes.OK).json(ticketHistroy);
 };
 
 module.exports = {
