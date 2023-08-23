aed49a343dd34ebc316fc2b663c273b5a8962754
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2023-08-23 10:00:28 +0800

    Jwt中間層驗證 新增資料庫Token (#16)
    * Token Model
    
    * Create Token in Login Controller
    
    * Send Multiple Cookies
    
    * Check for Existing Token
    
    * Refactor Auth Middleware - AccessToken
    
    * logout
    
    * 更新

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const User = require('../models/User');
+const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookieToResponse, createTokenUser } = require('../utlis');
+const crypto = require('crypto');
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -38,16 +40,45 @@ const login = async (req, res) => {
   }
 
   const tokenUser = createTokenUser(user);
-  attachCookieToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  // create refresh token
+  let refreshToken = '';
+  // check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+
+    refreshToken = existingToken.refreshToken;
+    attachCookieToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
+  }
+  refreshToken = crypto.randomBytes(40).toString('hex');
+  const userAgent = req.headers['user-agent'];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  await Token.create(userToken);
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
+  await Token.findOneAndDelete({ user: req.user.userId });
+
+  res.cookie('accessToken', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now() + 5 * 1000),
+    expires: new Date(Date.now() + 1000),
   });
-  res.status(StatusCodes.OK).json({ msg: '成功登出！' });
+
+  res.cookie('refreshToken', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
 module.exports = { register, login, logout };
--- utlis/jwt.js
+++ utlis/jwt.js
@@ -3,25 +3,31 @@ const jwt = require('jsonwebtoken');
 
 // 創造 JWT
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
 // 認證 JWT
-const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
+const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
 // 將使用者資料 存在 cookie // user是tokenUser
-const attachCookieToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+const attachCookieToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
   const oneDay = 1000 * 60 * 60 * 24;
 
-  res.cookie('token', token, {
+  res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
+    maxAge: 1000 * 60 * 15,
+  });
+
+  res.cookie('refreshToken', refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+    expires: new Date(Date.now() + oneDay),
   });
 };
 
