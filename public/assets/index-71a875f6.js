161771f2ccd2827ad50cff7abd5d508f97c5d06f
Asherbirdy <birdym.tw@gmail.com>
2023-09-09 14:45:05 +0800

    前端部署：修改票券歷史

--- public/assets/index-71a875f6.js
+++
@@ -1,1 +0,0 @@
-function r(t){return new Date(t).toISOString().split("T")[0]}const n=(t,e)=>{if(t==="used")return"#B3C3C5";if(t==="unuse"){if(e==="成人票")return"#00B9D2";if(e==="兒童票")return"#4786D0";if(e==="優待票")return"#04D200"}return""},o=r,a=n;export{a,o as g};
