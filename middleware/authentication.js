f8294c18a36174a161c51a5d87ea1064b1974ca0
Asher Lien <asher.lien@riversoft.com.tw>
2024-03-05 11:13:21 +0800

    更新

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,21 +1,21 @@
-const CustomError = require('../errors');
-const { isTokenValid, attachCookieToResponse } = require('../utlis');
+const CustomError = require("../errors");
+const { isTokenValid, attachCookieToResponse } = require("../utlis");
 
 const authenticateUser = async (req, res, next) => {
-  let token = null
+  let token = null;
   const { refreshToken, accessToken } = req.signedCookies;
-  
-  if (accessToken){ 
-    console.log('cookie的token')
-    token = accessToken
-  }
 
-  const authHeader = req.headers['authorization'];
-  const authToken = authHeader.split(' ')[1];
+  if (accessToken) {
+    console.log("cookie的token");
+    token = accessToken;
+  } else {
+    const authHeader = req.headers["authorization"];
+    const authToken = authHeader.split(" ")[1];
 
-  if(authHeader){
-    console.log('authHeader的token')
-    token = authToken
+    if (authHeader) {
+      console.log("authHeader的token");
+      token = authToken;
+    }
   }
 
   try {
@@ -31,7 +31,7 @@ const authenticateUser = async (req, res, next) => {
       refreshToken: payload.refreshToken,
     });
     if (!existingToken || !existingToken?.isValid) {
-      throw new CustomError.UnauthenticatedError('Authentication Invalid');
+      throw new CustomError.UnauthenticatedError("Authentication Invalid");
     }
     attachCookieToResponse({
       res,
@@ -40,7 +40,7 @@ const authenticateUser = async (req, res, next) => {
     });
     req.user = payload.user;
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
 };
 
@@ -48,7 +48,7 @@ const authorizePermission = (...role) => {
   return (req, res, next) => {
     if (!role.includes(req.user.role)) {
       throw new CustomError.UnauthenticatedError(
-        'Unauthorized to access this route'
+        "Unauthorized to access this route"
       );
     }
     next();
