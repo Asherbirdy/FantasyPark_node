7754e691e85dabc0837a1c4db287b752ee969766
Asher Lien <asher.lien@riversoft.com.tw>
2024-03-07 14:49:02 +0800

    完成api

--- controllers/ticketCategoryController.js
+++ controllers/ticketCategoryController.js
@@ -1,6 +1,6 @@
-const TicketCategory = require('../models/TicketCategory');
-const { StatusCodes } = require('http-status-codes');
-const CustomError = require('../errors');
+const TicketCategory = require("../models/TicketCategory");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const getAllTicketTypes = async (req, res) => {
   const getAllTicketTypes = await TicketCategory.find({});
@@ -14,7 +14,7 @@ const createTicketType = async (req, res) => {
   const { ticketType, fastTrack, price, description } = req.body;
 
   if (!ticketType || !price) {
-    throw new CustomError.BadRequestError('Please provide ticketType, price');
+    throw new CustomError.BadRequestError("Please provide ticketType, price");
   }
 
   const ticketAlreadyExist = await TicketCategory.findOne({
@@ -44,11 +44,42 @@ const updateTicketType = async (req, res) => {
 };
 
 const getActiveTicketType = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg:'getActiveTicketType' });
+  let { page, limit } = req.body;
+  page = parseInt(page, 10) || 1;
+  limit = parseInt(limit, 10) || 10;
+  const skip = (page - 1) * limit;
+
+  const ticketType = await TicketCategory.find({ active: true })
+    .skip(skip)
+    .limit(limit);
+  const total = await TicketCategory.countDocuments({ active: true });
+
+  res.status(StatusCodes.OK).json({
+    activeTicket: ticketType,
+    page,
+    totalPages: Math.ceil(total / limit),
+    totalItems: total,
+  });
 };
 
 const getNonActiveTicketType = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg:'getNonActiveTicketType' });
+  let { page, limit } = req.body;
+
+  page = parseInt(page, 10) || 1;
+  limit = parseInt(limit, 10) || 10;
+  const skip = (page - 1) * limit;
+
+  const ticketType = await TicketCategory.find({ active: false })
+    .skip(skip)
+    .limit(limit);
+  const total = await TicketCategory.countDocuments({ active: false });
+
+  res.status(StatusCodes.OK).json({
+    nonActiveTicket: ticketType,
+    page,
+    totalPages: Math.ceil(total / limit),
+    totalItems: total,
+  });
 };
 
 module.exports = {
@@ -56,5 +87,5 @@ module.exports = {
   createTicketType,
   updateTicketType,
   getActiveTicketType,
-  getNonActiveTicketType
+  getNonActiveTicketType,
 };
